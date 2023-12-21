9c5ed7daab3f297acc7184ce2a3c0b4b97c02eda
jlanzarotta <jlanzarotta@users.noreply.github.com>
2023-12-20 22:55:40 -0500

    Updated build.bat to include the version number.

--- build.bat
+++ build.bat
@@ -2,5 +2,6 @@
 set CGO_ENABLED=1
 set GOOS=windows
 set GOARCH=amd64
+set BUILD_VERSION=1.0.4
 set BUILD_DATETIME="%date:~10,4%-%date:~4,2%-%date:~7,2%T%time: =0%"
-go build -ldflags="-s -w -X 'timetracker/cmd.BuildDateTime=%BUILD_DATETIME%'" -work -x -v -o tt.exe main.go
+go build -ldflags="-s -w -X 'timetracker/cmd.BuildDateTime=%BUILD_DATETIME%' -X 'timetracker/cmd.BuildVersion=%BUILD_VERSION%'" -work -x -v -o tt.exe main.go
