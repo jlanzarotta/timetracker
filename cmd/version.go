fb0146934a25e043a414e9445b2dfb01b5a06bd2
Jeff Lanzarotta <jeff.lanzarotta@gmail.com>
2024-05-08 12:49:01 -0400

    Code cleanup.

--- cmd/version.go
+++ cmd/version.go
@@ -10,11 +10,11 @@ import (
 var BuildVersion string
 var BuildDateTime string
 
-// addCmd represents the add command
+// versionCmd represents the version command
 var versionCmd = &cobra.Command{
-	Use:    "version",
-	Short:  "Show the version information",
-	Long:   "Show the version information",
+	Use:   "version",
+	Short: "Show the version information",
+	Long:  "Show the version information",
 	Run: func(cmd *cobra.Command, args []string) {
 		runVersion(cmd, args)
 	},
